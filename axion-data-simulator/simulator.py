caf80a75f7964d71476ea2aad4a48215c885ad38
BrijendraYadav-dev <brijendrayadav2009@gmail.com>
2026-09-19 21:06:07 +0530

    codde modified

--- axion-data-simulator/simulator.py
+++ axion-data-simulator/simulator.py
@@ -8,7 +8,7 @@ from datetime import datetime, timezone
 # Configuration
 # ---------------------------------------------------------------------------
 # Point to the ingestion service
-API_URL = os.getenv("API_URL", "http://ingestion.krishomtrendstore.shop/api/v1/telemetry/ingest")
+API_URL = os.getenv("API_URL", "http://axion-ingestion-service:8080/api/v1/telemetry/ingest")
 INTERVAL_SECONDS = int(os.getenv("INTERVAL_SECONDS", "5"))
 
 # ---------------------------------------------------------------------------
